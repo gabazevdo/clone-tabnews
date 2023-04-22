f76f28b96e98b68e8545258577585ce794567a05
Gabriel Azevedo <gbldja@gmail.com>
2023-04-22 18:43:09 +0000

    chore: alterando e inserindo texto no index

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
 function Home() {
   return (
     <>
-      <h1>Teste 2</h1>
+      <h1>
+        Vocé é tão especial
+        <br />
+        que deveria ser inserida ao dicionário Aurélio
+      </h1>
+      <br />
+      <h3>Lais</h3>
+      <p>
+        <q>
+          Que sente e expressa amor, carinhosa, meiga, charmosa, linda e
+          maravilhosa.
+        </q>
+      </p>
     </>
   )
 }
